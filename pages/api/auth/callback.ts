6251e1f2885f5b87b924de4f21a8369f78089a99
arinoach <arinoach@gmail.com>
2025-11-26 22:41:15 +0800

    fix: 修复auth/callback.ts的import路径

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { supabaseAdmin } from '../../lib/supabase'
+import { supabaseAdmin } from '../../../lib/supabase'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { code, shop } = req.query
